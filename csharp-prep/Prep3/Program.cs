f0586a365359f81fedc38351a32d8fb17db8d424
terdeck <taeraesuu@gmail.com>
2023-11-08 14:47:42 -0500

    update c# prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("What is the magic number? ");
+        // string number = Console.ReadLine();
+        // int magicNumber = int.Parse(number);
+
+        Random randomGenerate = new Random();
+        int magicNumber = randomGenerate.Next(1, 101);
+
+        int guessInput = -1;
+
+        while (guessInput != magicNumber)
+        {
+            Console.WriteLine("What is your guess? ");
+            string guess = Console.ReadLine();
+            guessInput = int.Parse(guess);
+
+            if (guessInput < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessInput > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+            // Console.WriteLine(magicNumber);
+        }
     }
 }
