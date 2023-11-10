b0749c3c5b05f9a63d57b4e2559d35f4b54ae477
terdeck <taeraesuu@gmail.com>
2023-11-10 10:55:53 -0500

    update c# prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -28,7 +28,7 @@ class Program
     }
     static int PromptUserNumber()
     {
-        Console.WriteLine("Please enter your favorite number: ");
+        Console.Write("Please enter your favorite number: ");
         int favNumber = int.Parse(Console.ReadLine());
 
         return favNumber;
